17f8777d8a8bdce43dc8a8b4066dd03d7337fcc6
HelitonRodrigues <heliton@silicate.com.br>
2026-04-13 14:48:37 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -3,7 +3,7 @@
 // Cliente Supabase único para todos os módulos
 // ============================================================
 
-const SUPABASE_URL = 'https://cskcvntadptysfrvgczj.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_f5WXhVto_-zoX6Vrg33tRg_ooiIYh5c';
+const SUPABASE_URL = 'https://zodgbitbflkepbszshmu.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_ioEty5Ly9AeNzDHemcV_Hw_uhIejLj6';
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
