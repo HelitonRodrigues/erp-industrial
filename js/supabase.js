cc8a1ac75b12fa607f0bbfb1e7100c64f2737377
HelitonRodrigues <heliton@silicate.com.br>
2026-04-26 00:15:01 -0300

    Add files via upload

--- js/supabase.js
+++ js/supabase.js
@@ -6,4 +6,7 @@
 const SUPABASE_URL = 'https://zodgbitbflkepbszshmu.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvZGdiaXRiZmxrZXBic3pzaG11Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NjA2NjA2OCwiZXhwIjoyMDkxNjQyMDY4fQ.cclkLlP8v7IMH4AXGliDNxBEyn5Sg2VxyHIEEsv_yc8';
 
-const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY, {
+  realtime: { enabled: false },
+  global: { headers: { 'x-client-info': 'erp-industrial' } }
+});
